fc48529f91bc4e087082f39ed9c5c42ba8a09ab2
hrshihab <hrshihab10@gmail.com>
2023-12-12 15:53:58 +0600

    lint added

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
